d2f91a4668486026545bc891082dfdb15a8c3385
eileen <eileen@eileen.com>
2017-04-30 02:39:00 +0200

    changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,28 @@
 var a = prompt('Podaj wartosc a:'),
-	b = prompt('Podaj wartosc b:'),
-	value= (a * a) + (2 * a * b) - (b * b);
+	b = prompt('Podaj wartosc b:');
 
-console.log('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value);
-
-alert('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value)
-
-if (value > 0) {
-	console.log('Wynik jest dodatni');
-	alert('Wynik jest dodatni'); 
-} else if (value < 0) {
-	console.log('Wynik jest ujemny');
-	alert('Wynik jest ujemny');
+if (isNaN(a)) {
+	console.log('Nieprawidlowa wartosc a');
+	alert('Nieprawidlowa wartosc a');
+} else if (isNaN(b)) {
+	console.log('Nieprawidlowa wartosc b');
+	alert('Nieprawidlowa wartosc b');
 } else {
-	console.log('Wynik jest rowny 0');
-	alert('Wynik jest rowny 0');
+
+	var value= (a * a) + (2 * a * b) - (b * b);
+
+	console.log('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value);
+
+	alert('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value)
+
+	if (value > 0) {
+		console.log('Wynik jest dodatni');
+		alert('Wynik jest dodatni'); 
+	} else if (value < 0) {
+		console.log('Wynik jest ujemny');
+		alert('Wynik jest ujemny');
+	} else {
+		console.log('Wynik jest rowny 0');
+		alert('Wynik jest rowny 0');
+	}
 }
