4b39adbc74e8d835359d521b3c76ca78fff7ece8
eileen <eileen@eileen.com>
2017-04-30 02:09:23 +0200

    adding files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+var a = prompt('Podaj wartosc a:'),
+	b = prompt('Podaj wartosc b:'),
+	value= (a * a) + (2 * a * b) - (b * b);
+
+console.log('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value);
+
+alert('Wynik dzialania (a * a) + (2 * a * b) - (b * b) wynosi: ' + value)
+
+if (value > 0) {
+	console.log('Wynik jest dodatni');
+	alert('Wynik jest dodatni'); 
+} else if (value < 0) {
+	console.log('Wynik jest ujemny');
+	alert('Wynik jest ujemny');
+} else {
+	console.log('Wynik jest rowny 0');
+	alert('Wynik jest rowny 0');
+}
